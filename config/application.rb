79fcde112bfd5123ea7997f2f1053456c0cdd668
liyijie <liyijie825@gmail.com>
2016-09-24 10:48:42 +0800

    use rack-cors gem for CORS api support

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,14 @@ module CloudClosetApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Rack::Cors provides support for Cross-Origin Resource Sharing (CORS) for Rack compatible web applications.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
   end
 end
