5420267f6cc6308cf78fdd1d78e6fd6664188fea
liyijie <liyijie825@gmail.com>
2016-09-24 23:12:24 +0800

    set timezone to Beijing, default_locale to zh-CN

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,13 @@ module CloudClosetApi
 
     config.action_controller.asset_host = "http://closet-api.tallty.com"
 
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Beijing'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :'zh-CN'
+
   end
 end
