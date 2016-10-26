1a1b0d68c3dea4b46f3b4296393946eb074ed95c
ivan Lan <mushu@ACA80108.ipt.aol.com>
2016-10-26 22:07:48 +0800

    fix bug from lan's first push

--- spec/models/user_info_spec.rb
+++ spec/models/user_info_spec.rb
@@ -4,5 +4,5 @@ RSpec.describe UserInfo, type: :model do
   it { should belong_to(:user) } 
   it { should have_one(:avatar) } 
   it { should have_many(:addresses) } 
-  it { should have_many(:purchase_log) } 
+  it { should have_many(:purchase_logs) } 
 end
