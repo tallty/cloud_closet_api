346010b72f98e4a5424d118ba911c77d6fdf9b79
ivan Lan <mushu@ACA80107.ipt.aol.com>
2016-11-02 16:16:07 +0800

    add ping++ key && try to fix default?

---
+++ config/initializers/pingxx.rb
@@ -0,0 +1,13 @@
+require "pingpp"
+
+# api_key 获取方式：登录 [Dashboard](https://dashboard.pingxx.com)->点击管理平台右上角公司名称->开发信息-> Secret Key
+API_KEY = "sk_test_Te9SK0Cqn1yTy5OeDC104KGS"
+# app_id 获取方式：登录 [Dashboard](https://dashboard.pingxx.com)->点击你创建的应用->应用首页->应用 ID(App ID)
+APP_ID = "app_TCGezLWzLe1K5Oar"
+# 设置 API key
+Pingpp.api_key = API_KEY
+# 设置你的私钥路径，用于请求的签名
+# 私钥生成 https://help.pingxx.com/article/123161/
+Pingpp.private_key_path = File.dirname(__FILE__) + '/../../your_rsa_private_key.pem'
+
+Pingpp.pub_key_path =  File.dirname(__FILE__) + '/../../pingpp_rsa_public_key.pem'
