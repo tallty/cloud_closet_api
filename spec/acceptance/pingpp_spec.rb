2fb0e5df92c51705a028684697e6f55de0cfbafd
Ivan Lan <mumumumushu@gmail.com>
2016-11-05 17:39:05 +0800

    change wechat message template

--- spec/acceptance/pingpp_spec.rb
+++ spec/acceptance/pingpp_spec.rb
@@ -10,13 +10,14 @@ resource "ping++平台支付相关" do
   	parameter :subject, "充值/消费/提现", required: true
   	parameter :body, "余额充值/购买衣橱/衣服配送/余额提现", required: true
   	parameter :metadata, '其他描述，键值对，例：\'{"detail":"裤子*2 衣服*4"}\'', required: false
+    
 
     let(:channel) { 'wx_pub' }
     let(:openid) { "olclvwHtOBENZ-rLA2NxsBCVZky0" }
     let(:amount) { "100" }
     let(:subject) { "充值" }
     let(:body) { "余额充值" }
-    let(:metadata) { '{"detail":"裤子*2 衣服*4"}' }
+    # let(:metadata) { '{"detail":"裤子*2 衣服*4"}' }
 
     example "向ping++平台请求支付凭证" do
       do_request
