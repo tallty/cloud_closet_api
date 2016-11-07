f44bbb2f4434d8f97b051e25e9abd2c7846a8693
tangmingchao <18798805737@139.com>
2016-11-07 17:21:27 +0800

    edit 'app/controllers/work/appointments_controller.rb and spec/acceptance/work_spec.rb'

--- spec/acceptance/work_spec.rb
+++ spec/acceptance/work_spec.rb
@@ -42,11 +42,11 @@ resource "工作台相关接口" do
       @worker = create(:worker)
       @user = create(:user)
       create(:user_info, user: @user)
-      today_appointments = create_list(:appointment, 3, user: @user, date: Time.zone.today)
-      tomorrow_appointments = create_list(:appointment, 3, user: @user, date: Time.zone.today + 1.days)
+      today_appointments = create_list(:appointment, 1, user: @user, date: Time.zone.today)
+      tomorrow_appointments = create_list(:appointment, 1, user: @user, date: Time.zone.today + 1.days)
       @appointments = today_appointments.concat tomorrow_appointments
       @appointments.each do |appointment|
-        @groups = create_list(:appointment_item_group, 3, appointment: appointment)
+        @groups = create_list(:appointment_item_group, 1, appointment: appointment)
       end
     end
 
@@ -97,8 +97,6 @@ resource "工作台相关接口" do
       let(:tag_list) { 6 }
       let(:appointment_id) { @appointments.first.id }
       
-
-
       example "工作人员修改指定订单下面的订单组成功" do
         params = {
           appointment_item: 
@@ -140,57 +138,57 @@ resource "工作台相关接口" do
       end
     end
                           #######
-    get 'admin/appointment_item_groups/:appointment_item_group_id/garments' do
-      let(:appointment_item_group_id) { @groups.first.id }
-      example "管理员获取指定订单下面的订单组对应的衣服列表成功" do
-        do_request
-        puts response_body
-        expect(status).to eq(200)
-      end
-    end
+    # get 'admin/appointment_item_groups/:appointment_item_group_id/garments' do
+    #   let(:appointment_item_group_id) { @groups.first.id }
+    #   example "管理员获取指定订单下面的订单组对应的衣服列表成功" do
+    #     do_request
+    #     puts response_body
+    #     expect(status).to eq(200)
+    #   end
+    # end
 
-    put 'admin/garments/:id' do
-      image_attrs = FactoryGirl.attributes_for(:image, photo_type: "avatar")
+    # put 'admin/garments/:id' do
+    #   image_attrs = FactoryGirl.attributes_for(:image, photo_type: "avatar")
 
-      parameter :title, "衣服描述信息", require: false, scope: :garment
-      parameter :row, "衣服存放的 排 ", require: false, scope: :garment
-      parameter :carbit, "衣服存放的 柜 ", require: false, scope: :garment
-      parameter :place, "衣服存放的 位 ", require: false, scope: :garment
-      parameter :cover_image_attributes, "衣服的封面图", require: false, scope: :garment
+    #   parameter :title, "衣服描述信息", require: false, scope: :garment
+    #   parameter :row, "衣服存放的 排 ", require: false, scope: :garment
+    #   parameter :carbit, "衣服存放的 柜 ", require: false, scope: :garment
+    #   parameter :place, "衣服存放的 位 ", require: false, scope: :garment
+    #   parameter :cover_image_attributes, "衣服的封面图", require: false, scope: :garment
 
-      let(:id) { @groups.first.garments.first.id }
-      let(:title) { "garemnt title" }
-      let(:row) { 1 }
-      let(:carbit) { 3 }
-      let(:place) { 2 }
-      let(:cover_image_attributes) { image_attrs }
+    #   let(:id) { @groups.first.garments.first.id }
+    #   let(:title) { "garemnt title" }
+    #   let(:row) { 1 }
+    #   let(:carbit) { 3 }
+    #   let(:place) { 2 }
+    #   let(:cover_image_attributes) { image_attrs }
 
-      example "管理员完善衣服的详细信息成功" do
-        do_request
-        puts response_body
-        expect(status).to eq 200
-      end
-    end
+    #   example "管理员完善衣服的详细信息成功" do
+    #     do_request
+    #     puts response_body
+    #     expect(status).to eq 200
+    #   end
+    # end
 
-    get 'admin/garments/:id' do
-      let(:id) { @groups.first.garments.first.id }
+    # get 'admin/garments/:id' do
+    #   let(:id) { @groups.first.garments.first.id }
 
-      example "管理员查看衣服的详细信息成功" do
-        do_request
-        puts response_body
-        expect(status).to eq 200
-      end 
-    end
+    #   example "管理员查看衣服的详细信息成功" do
+    #     do_request
+    #     puts response_body
+    #     expect(status).to eq 200
+    #   end 
+    # end
   end
 
   get '/work/appointments/state_query' do
 
-      worker_attrs = FactoryGirl.attributes_for(:worker)
+    worker_attrs = FactoryGirl.attributes_for(:worker)
 
-      header "X-User-Token", worker_attrs[:authentication_token]
-      header "X-User-Phone", worker_attrs[:phone]
+    header "X-User-Token", worker_attrs[:authentication_token]
+    header "X-User-Phone", worker_attrs[:phone]
 
-      before do
+    before do
       @worker = create(:worker)
       @user = create(:user)
       create(:user_info, user: @user)
