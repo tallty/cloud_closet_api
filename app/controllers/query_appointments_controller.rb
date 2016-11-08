cd05e05a2bea0b43e601e8b614220e55b6861a75
ivan Lan <mushu@ACA80105.ipt.aol.com>
2016-11-08 16:29:09 +0800

    add cancel for appointment in user_appointment_controller && fix bug in query_appointment

--- app/controllers/query_appointments_controller.rb
+++ app/controllers/query_appointments_controller.rb
@@ -3,8 +3,9 @@ class QueryAppointmentsController < ApplicationController
 	#参数 状态 逗号隔开
 	#committed,accepted,unpaid,paid,storing,stored,canceled
 	def query_appointments
-		_state_array = params[:state].split(",") || [committed,accepted,unpaid,paid,storing,stored,canceled]
- 
+		_state = 
+		_state_array = params[:state] ? params[:state].split(",") : ["committed","accepted","unpaid","paid","storing","stored","canceled"]
+
 		@query_result = Appointment.appointment_state(_state_array)
 		
 		page = params[:page] || 1
