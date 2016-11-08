c388b6cc5b8aa4dcee5cedb440cebabd2e380548
Ivan Lan <mumumumushu@gmail.com>
2016-11-08 19:22:37 +0800

    change work_appointment#index output

--- app/views/work/appointments/index.json.jbuilder
+++ app/views/work/appointments/index.json.jbuilder
@@ -1,3 +1,5 @@
+json.user_count User.user_count
+json.new_user_count_today User.new_user_count_today
 
 json.appointments @appointments_hash do |date, appointments|
   json.date date
--- spec/acceptance/query_appointments_spec.rb
+++ spec/acceptance/query_appointments_spec.rb
@@ -6,8 +6,8 @@ resource "按条件查询预约订单" do
   get 'query_appointments' do
 
   	parameter :state, "查询条件,英文字符串，使用逗号隔开每个条件，committed,accepted,unpaid,paid,storing,stored,canceled", required: false
-  	parameter :page, "当前页", require: false
-    parameter :per_page, "每页的数量", require: false
+  	parameter :page, "当前页", required: false
+    parameter :per_page, "每页的数量", required: false
 
   	before do
 	  	@user = create(:user)
