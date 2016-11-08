c3682dfafdeda53e016968bcbaa0bc90c8e5d4f1
Ivan Lan <mumumumushu@gmail.com>
2016-11-08 09:05:46 +0800

    set default condition for query appointment

--- app/controllers/query_appointments_controller.rb
+++ app/controllers/query_appointments_controller.rb
@@ -3,7 +3,7 @@ class QueryAppointmentsController < ApplicationController
 	#参数 状态 逗号隔开
 	#committed,accepted,unpaid,paid,storing,stored,canceled
 	def query_appointment
-		_state_array = params[:state].split(",")
+		_state_array = params[:state].split(",") || [committed,accepted,unpaid,paid,storing,stored,canceled]
  
 		@query_result = Appointment.appointment_state(_state_array)
 		
