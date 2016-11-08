ffc144172bfb61f6ba17266c3a2efdf033271fd9
ivan Lan <mushu@ACA80105.ipt.aol.com>
2016-11-08 09:05:46 +0800

    set default condition for query appointment

--- spec/acceptance/query_appointments_spec.rb
+++ spec/acceptance/query_appointments_spec.rb
@@ -5,7 +5,7 @@ resource "按条件查询预约订单" do
 
   get 'query_appointments' do
 
-  	parameter :state, "查询条件,英文字符串，使用逗号隔开每个条件，committed,accepted,unpaid,paid,storing,stored,canceled", required: true
+  	parameter :state, "查询条件,英文字符串，使用逗号隔开每个条件，committed,accepted,unpaid,paid,storing,stored,canceled", required: false
   	parameter :page, "当前页", require: false
     parameter :per_page, "每页的数量", require: false
 
@@ -22,7 +22,7 @@ resource "按条件查询预约订单" do
 
 		let(:state) {"committed,accepted,unpaid,storing"}
 
-		example "按条件查询预约订单成功" do
+		example "按条件查询预约订单成功,默认查询所有" do
       do_request
       puts response_body
       expect(status).to eq(200)
