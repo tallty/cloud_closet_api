7f8d2041b3f97f0400cb7bafc76ed5157bdc5eae
mumumumushu <mumumumushu@gmail.com>
2017-03-03 17:17:27 +0800

    let exhibition_unit can be get in admin#price_system#index

--- app/controllers/admin/price_systems_controller.rb
+++ app/controllers/admin/price_systems_controller.rb
@@ -12,27 +12,27 @@ class Admin::PriceSystemsController < ApplicationController
     page = params[:page] || 1
     per_page = params[:per_page] || 10
     @price_systems = PriceSystem.all.paginate(page: page, per_page: per_page)
-    respond_with @price_systems, template: "price_systems/index", status: 200
+    respond_with @price_systems, template: "worker_price_systems/index", status: 200
   end
 
   def show
-    respond_with @price_system, template: "price_systems/show", status: 200
+    respond_with @price_system, template: "worker_price_systems/show", status: 200
   end
 
   def create
     @price_system = PriceSystem.new(price_system_params)
     @price_system.save
-    respond_with @price_system, template: "price_systems/show", status: 201
+    respond_with @price_system, template: "worker_price_systems/show", status: 201
   end
 
   def update
     @price_system.update(price_system_params)
-    respond_with @price_system, template: "price_systems/show", status: 201
+    respond_with @price_system, template: "worker_price_systems/show", status: 201
   end
 
   def destroy
     @price_system.destroy
-    respond_with @price_system, template: "price_systems/show", status: 204
+    respond_with @price_system, template: "worker_price_systems/show", status: 204
   end
 
   private
