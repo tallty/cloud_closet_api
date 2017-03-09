a78d5d22976712a5f056ac6b53ae722f50c73051
mumumumushu <mumumumushu@gmail.com>
2017-03-09 16:03:01 +0800

    add wechat_message_service && update the msg be send when appt state change

--- app/services/wechat_message_service.rb
+++ app/services/wechat_message_service.rb
@@ -1,58 +1,68 @@
-# class WechatMessageService
+class WechatMessageService
 
-# 	def in
-		
-# 	end
+	def initialize user
+		@openid = user.try(:openid)
+		# @openid = 'olclvwHtOBENZ-rLA2NxsBCVZky0'
+		raise '用户openid 为空' if openid.blank?
+		@template = {}
+	end
 
+	def appt_state_msg appt
+    appt_state_remark = {
+			committed: '工作人员即将与您联系确认订单。', 
+			accepted: '请您留意上门时间，作好合理安排。', 
+			unpaid: '您的订单已生成，请尽快完成付款', 
+			paid: '如有疑问，敬请咨询客服热线：15800634815',
+			storing: '如有疑问，敬请咨询客服热线：15800634815', 
+			stored: '如有疑问，敬请咨询客服热线：15800634815', 
+			canceled: ''
+		}
 
-# 	def aaa
-#     openid = 'olclvwHtOBENZ-rLA2NxsBCVZky0'
-#     return if openid.blank?
-#     template = {
-#       template_id: "6M5zwt6mJeqk6E29HnVj2qdlyA68O9E-NNP4voT1wBU",
-#       url: "http://closet.tallty.com/orders",
-#       topcolor: "#FF0000",
-#       data: {
-#         first: {
-#           value: "亲，您的预约订单状态变更，请注意查看。",
-#           color: "#0A0A0A"
-#         },
-#         keyword1: {
-#           value: "乐存好衣",
-#           color: "#CCCCCC"
-#         },
-#         # keyword2: {
-#         #   value: 111,#商家电话？？？？！！！！
-#         #   color: "#CCCCCC"
-#         # },
-#         keyword3: {
-#           value: 111,
-#           color: "#CCCCCC"
-#         },
-#         keyword4: {
-#           value: 111,
-#           color: "#CCCCCC"
-#         },
-#         keyword5: {
-#           value: "上门评估",
-#           color: "#CCCCCC"
-#         },
-#         remark: {
-#           value: "请您留意上门时间，合理安排好您的时间",
-#           color: "#173177"
-#         }
-#       }
-#     }
-#     # if state == 'stored'
-#     #   template[:url] = 'http://closet.tallty.com/MyCloset' 
-#     #   template[:data][:keyword3] = nil
-#     #   template[:data][:remark] = nil
-#     # end
+    template = {
+      template_id: "6M5zwt6mJeqk6E29HnVj2qdlyA68O9E-NNP4voT1wBU",
+      url: "http://closet.tallty.com/orders",
+      topcolor: "#FF0000",
+      data: {
+        first: {
+          value: "亲，您的预约订单状态变更，请注意查看。",
+          color: "#0A0A0A"
+        },
+        keyword1: {
+          value: "乐存好衣",
+          color: "#757575"
+        },
+        keyword2: 
+        {
+          value: "15800634815",
+          color: "#757575"
+        },
+        keyword3: {
+          value: appt.seq,
+          color: "#757575"
+        },
+        keyword4: {
+          value: appt.state,
+          color: "#757575"
+        },
+        keyword5: {
+          value: "#{appt.price == 0 ? "上门评估" : appt.price}",
+          color: "#757575"
+        },
+        remark: {
+          value: appt_state_remark[ appt.aasm_state.to_sym ],
+          color: "#173177"
+        }
+      }
+    }
+    template[:url] = 'http://closet.tallty.com/MyCloset' if appt.aasm_state == 'stored'
     
-#     response = Faraday.post 'http://wechat-api.tallty.com/cloud_closet_wechat/template_message',
-#       { openid: openid, template: template}
-#     puts response.body
-#   end
+    send_msg
+  end
 
+  def send_msg
+  	response = Faraday.post 'http://wechat-api.tallty.com/cloud_closet_wechat/template_message',
+      { openid: @openid, template: @template}
+    puts response.body
+  end
 
-# end
+end
