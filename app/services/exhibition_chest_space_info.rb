7a84a49aad44299d7fd014a2d7d2e16660414b9c
mumumumushu <mumumumushu@gmail.com>
2017-03-09 17:44:08 +0800

    fix exhi chest space_info's remain_space

--- app/services/exhibition_chest_space_info.rb
+++ app/services/exhibition_chest_space_info.rb
@@ -1,33 +1,27 @@
 module ExhibitionChestSpaceInfo
   # 重复查询？？how?
   def max_space_count
-  	_chests = ExhibitionChest.those_buddies_need_join_by(self)
   	self.need_join ?
-  		_chests.map(&:max_count).reduce(:+) :
+  		ExhibitionChest.those_buddies_need_join_by(self).map(&:max_count).reduce(:+) :
   		self.max_count
 		rescue
-			nil
+			self.try(:max_count)
 	end
 
 	def be_token_space_count
-		_chests = ExhibitionChest.those_buddies_need_join_by(self)
 		self.need_join ?
-			_chests.map { |chest| chest.garments.count }.reduce(:+) :
+			ExhibitionChest.those_buddies_need_join_by(self).map { |chest| chest.garments.count }.reduce(:+) :
 			self.garments.count
 		rescue
-			nil
+			self.garments.count
 	end
 
 	def remain_space_count
-		self.max_count - self.be_token_count
-		rescue
-			nil
+		self.max_count - self.be_token_space_count
 	end
 
 	def it_has_space
     remain_space_count > 0
-    rescue
-			nil
   end
 
 end
