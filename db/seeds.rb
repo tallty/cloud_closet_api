0d9b671af503492bac63999b1eac73a5674b766f
mumumumushu <mumumumushu@gmail.com>
2017-03-16 14:03:00 +0800

    change detail of price_system in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,63 +14,63 @@ StoreMethod.create!(
 		{ id: 3, title: "full_dress", zh_title: "礼服"},
 		])	
 
-# PriceSystem.destroy_all
-# PriceSystem.create!(
-# 	[
-# 		{ id: 1, title: '叠放柜', price: 180, description: '叠放柜可存放针织类，卫衣棉服等可折叠衣物60件, 也可提供真空袋出售', is_chest: true, unit_name: '月' },
-# 		{ id: 2, title: '挂柜', price: 180, description: '叠放柜可存放针织类，卫衣棉服等可折叠衣物60件, 也可提供真空袋出售', is_chest: true, unit_name: '月' },
-# 		{ id: 3, title: '组合柜', price: 400, description: '*组合柜可存放60件折叠和20件挂放', is_chest: true, unit_name: '月' },
-# 		{ id: 4, title: '单件礼服', price: 60, description: '我是单件礼服 ,每月60', is_chest: true, unit_name: '月' },
-# 		{ id: 5, title: '礼服柜', price: 600, description: '	礼服柜可存放12件贵重礼服，适合存放大件礼服；', is_chest: true, unit_name: '月' },
-# 		{ id: 6, title: '真空袋-中', price: 10, description: '我是真空袋中号', is_chest: false, unit_name: '个' },
-# 		{ id: 7, title: '真空袋-大', price: 10, description: '我是真空袋大号', is_chest: false, unit_name: '个' }
-# 		])		
-# ExhibitionUnit.destroy_all
-# ExhibitionUnit.create!  (
-# 	[
-# 		{ id: 1, title: "叠放柜", store_method: "stacking", max_count: 60, need_join: false, price_system_id: 1 },
-# 		{ id: 2, title: "挂柜", store_method: "hanging", max_count: 20, need_join: false, price_system_id: 2 },
-# 		{ id: 3, title: "组合柜-叠放柜", store_method: "stacking", max_count: 60, need_join: false, price_system_id: 3 },
-# 		{ id: 4, title: "组合柜-挂柜", store_method: "hanging", max_count: 20, need_join: false, price_system_id: 3 },
-# 		{ id: 6, title: "单件礼服", store_method: 'full_dress', max_count: 1, need_join: true, price_system_id: 4 },
-# 		{ id: 5, title: "礼服柜", store_method: 'full_dress', max_count: 12, need_join: false, price_system_id: 5 },
+PriceSystem.destroy_all
+PriceSystem.create!(
+	[
+		{ id: 1, title: '叠放柜', price: 180, description: '叠放柜可存放针织类，卫衣棉服等可折叠衣物60件, 也可提供真空袋出售', is_chest: true, unit_name: '月' },
+		{ id: 2, title: '挂柜', price: 180, description: '挂放柜可存放16件套装衣物，适合存放外套、大衣等大件衣物', is_chest: true, unit_name: '月' },
+		{ id: 3, title: '组合柜', price: 400, description: '组合柜可存放60件折叠和16件挂放衣物，适合灵活存挂衣物', is_chest: true, unit_name: '月' },
+		{ id: 4, title: '单件礼服', price: 60, description: '我是单件礼服 ,每月60', is_chest: true, unit_name: '月' },
+		{ id: 5, title: '礼服柜', price: 600, description: '	礼服柜可存放12件贵重礼服，适合存放大件礼服；', is_chest: true, unit_name: '月' },
+		{ id: 6, title: '真空袋-中', price: 10, description: '我是真空袋中号', is_chest: false, unit_name: '个' },
+		{ id: 7, title: '真空袋-大', price: 10, description: '我是真空袋大号', is_chest: false, unit_name: '个' }
+		])		
+ExhibitionUnit.destroy_all
+ExhibitionUnit.create!  (
+	[
+		{ id: 1, title: "叠放柜", store_method: "stacking", max_count: 60, need_join: false, price_system_id: 1 },
+		{ id: 2, title: "挂柜", store_method: "hanging", max_count: 16, need_join: false, price_system_id: 2 },
+		{ id: 3, title: "组合柜-叠放柜", store_method: "stacking", max_count: 60, need_join: false, price_system_id: 3 },
+		{ id: 4, title: "组合柜-挂柜", store_method: "hanging", max_count: 16, need_join: false, price_system_id: 3 },
+		{ id: 6, title: "单件礼服", store_method: 'full_dress', max_count: 1, need_join: true, price_system_id: 4 },
+		{ id: 5, title: "礼服柜", store_method: 'full_dress', max_count: 12, need_join: false, price_system_id: 5 },
 		
-# 		])
-RechargeRule.destroy_all
-RechargeRule.create! (
-	[
-		{ id: 1, amount: 1000, credits: 0 },
-		{ id: 2, amount: 2000, credits: 0 },
-		{ id: 3, amount: 3000, credits: 100 },
-		{ id: 4, amount: 5000, credits: 300 },
-		{ id: 5, amount: 10000, credits: 800 },
-		{ id: 6, amount: 20000, credits: 2000 },
-		{ id: 7, amount: 50000, credits: 6000 },
-		{ id: 8, amount: 100000, credits: 20000 },
-	])
+		])
+# RechargeRule.destroy_all
+# RechargeRule.create! (
+# 	[
+# 		{ id: 1, amount: 1000, credits: 0 },
+# 		{ id: 2, amount: 2000, credits: 0 },
+# 		{ id: 3, amount: 3000, credits: 100 },
+# 		{ id: 4, amount: 5000, credits: 300 },
+# 		{ id: 5, amount: 10000, credits: 800 },
+# 		{ id: 6, amount: 20000, credits: 2000 },
+# 		{ id: 7, amount: 50000, credits: 6000 },
+# 		{ id: 8, amount: 100000, credits: 20000 },
+# 	])
 
-ConstantTag.create (
-	[
-		{ title: '上衣', class_type: 'garment'},
-		{ title: '半裙', class_type: 'garment'},
-		{ title: '连衣裙', class_type: 'garment'},
-		{ title: '裤装', class_type: 'garment'},
-		{ title: '外套', class_type: 'garment'},
-		{ title: '羽绒服', class_type: 'garment'},
-		{ title: '泳装', class_type: 'garment'},
-		{ title: '春秋', class_type: 'garment'},
-		{ title: '冬', class_type: 'garment'},
-		{ title: '夏', class_type: 'garment'},	
-	])
-VipLevel.destroy_all
-VipLevel.create!(
-	[
-		{ name: '普通会员', rank: 1, exp: 0, birthday_gift: 30 },
-		{ name: '银卡会员', rank: 2, exp: 800, birthday_gift: 30 },
-		{ name: '金卡会员', rank: 3, exp: 6000, birthday_gift: 30 },
-		{ name: '砖石卡会员', rank: 4, exp: 20000, birthday_gift: 30 },
-	]
-	)
+# ConstantTag.create (
+# 	[
+# 		{ title: '上衣', class_type: 'garment'},
+# 		{ title: '半裙', class_type: 'garment'},
+# 		{ title: '连衣裙', class_type: 'garment'},
+# 		{ title: '裤装', class_type: 'garment'},
+# 		{ title: '外套', class_type: 'garment'},
+# 		{ title: '羽绒服', class_type: 'garment'},
+# 		{ title: '泳装', class_type: 'garment'},
+# 		{ title: '春秋', class_type: 'garment'},
+# 		{ title: '冬', class_type: 'garment'},
+# 		{ title: '夏', class_type: 'garment'},	
+# 	])
+# VipLevel.destroy_all
+# VipLevel.create!(
+# 	[
+# 		{ name: '普通会员', rank: 1, exp: 0, birthday_gift: 30 },
+# 		{ name: '银卡会员', rank: 2, exp: 800, birthday_gift: 30 },
+# 		{ name: '金卡会员', rank: 3, exp: 6000, birthday_gift: 30 },
+# 		{ name: '砖石卡会员', rank: 4, exp: 20000, birthday_gift: 30 },
+# 	]
+# 	)
 
 
 
