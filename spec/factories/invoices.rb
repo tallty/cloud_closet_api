cab9adbaa43ea4f5636235b604c6c41ccd333179
ivan Lan <mumumumushu@gmail.com>
2017-03-20 15:05:50 +0800

    create model invoice

---
+++ spec/factories/invoices.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :invoice do
+    title "MyString"
+    amount 1.5
+    type ""
+    aasm_state "MyString"
+    cel_name "MyString"
+    cel_phone "MyString"
+    postcode "MyString"
+    address "MyString"
+    date "2017-03-20"
+    blance 1.5
+    user ""
+  end
+end
