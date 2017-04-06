5870f0c259d99b19c9f08943e17c77d889a70965
ivan Lan <mumumumushu@gmail.com>
2017-04-06 18:47:46 +0800

    Add a pubilc_msg_image to PubilcMsg

--- app/models/publish_msg.rb
+++ app/models/publish_msg.rb
@@ -11,4 +11,6 @@
 #
 
 class PublishMsg < ApplicationRecord
+	has_one :pubilc_msg_image, -> { where photo_type: "pubilc_msg" }, class_name: "Image", as: :imageable, dependent: :destroy
+  accepts_nested_attributes_for :pubilc_msg_image, allow_destroy: true
 end
--- spec/factories/publish_msgs.rb
+++ spec/factories/publish_msgs.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :publish_msg do
-    title "MyString"
-    abstract "MyString"
+    title "公频通知 标题"
+    abstract "公频通知 概述"
+    content "公频通知 内容"
+    association :publish_msg_image, factory: :image, photo_type: "publish_msg"
   end
 end
