0f662bf11ccb4b2d8e74a469c6eb7126843bb0cd
ivan Lan <mumumumushu@gmail.com>
2017-05-03 14:48:37 +0800

    Fix migration bug

--- db/migrate/20170228094814_add_reference_to_garment_with_appointment.rb
+++ db/migrate/20170228094814_add_reference_to_garment_with_appointment.rb
@@ -3,6 +3,6 @@ class AddReferenceToGarmentWithAppointment < ActiveRecord::Migration[5.0]
   	add_reference :garments, :appointment, foreign: true
   	add_reference :garments, :exhibition_chest, foreign: true
 
-  	remove_reference :garments, :chest, foreign: true
+  	# remove_reference :garments, :chest, foreign: true
   end
 end
