77b273b9ef7504a567758affebc43151d28b4eb0
ivan Lan <mumumumushu@gmail.com>
2017-06-28 19:22:44 +0800

    Recover the rescue of admin#create servier_order

--- app/controllers/admin/service_orders_controller.rb
+++ app/controllers/admin/service_orders_controller.rb
@@ -30,8 +30,8 @@ class Admin::ServiceOrdersController < ApplicationController
     else
       head 404
     end
-  # rescue => error
-  #   render json: { error: error }, status: 422
+  rescue => error
+    render json: { error: error }, status: 422
   end
 
   def destroy
